24038bcc3d6afb338a62efacc5b788e1270e6cbf
Lawrence <lawrencecaj@gmail.com>
2022-08-24 13:19:08 -0400

    pages info

--- src/App.test.js
+++ src/App.test.js
@@ -1,1 +1,4 @@
 import '@testing-library/jest-dom/';
+
+
+// companion library for Testing Library that provides custom DOM element matchers for Jest
--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,4 +1,3 @@
-// allows you to measure performance of your app by looking at reacts supported metrics
 const reportWebVitals = onPerfEntry => {
     if (onPerfEntry && onPerfEntry instanceof Function) {
       import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
